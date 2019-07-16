14fd91e61849f1df31d6ff573706c5ea05574ef2
Lauren Le <laurenjle@fb.com>
2019-07-15 17:05:13 -0700

    added JobListing file under Models group

---
+++ seam/Models/JobListing.h
@@ -0,0 +1,22 @@
+//
+//  JobListing.h
+//  seam
+//
+//  Created by laurenjle on 7/15/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JobListing : NSObject
+//check available data to create properties
+//job name
+//job description
+//time slot for job
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ seam/Models/JobListing.m
@@ -0,0 +1,13 @@
+//
+//  JobListing.m
+//  seam
+//
+//  Created by laurenjle on 7/15/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import "JobListing.h"
+
+@implementation JobListing
+
+@end
