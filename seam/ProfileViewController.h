fe1d97a347177c5d90cbb3803b801b8e3e071aad
Alexa Morales <alexamorales@fb.com>
2019-07-18 13:57:24 -0700

    added profile view controller

---
+++ seam/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  seam
+//
+//  Created by alexamorales on 7/18/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
