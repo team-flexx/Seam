b1df89ce0965fd54a09d5003d8d5f05d4c67e532
festusojo123 <37356473+festusojo123@users.noreply.github.com>
2019-07-30 17:35:54 -0700

    Update segue in SignUpVC

--- seam/View Controllers/SignUpViewController.m
+++ seam/View Controllers/SignUpViewController.m
@@ -60,6 +60,7 @@
             NSLog(@"User registered successfully");
             
             // manually segue to logged in view
+            [self performSegueWithIdentifier:@"showMainView" sender:nil];
         }
     }];
 }
