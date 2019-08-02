a09ebd14ddf63eaa118348486881bf6d4dd706fc
Alexa Morales <alexamorales@fb.com>
2019-08-02 14:04:18 -0700

    matches view controller

--- seam/View Controllers/MatchesViewController.m
+++ seam/View Controllers/MatchesViewController.m
@@ -7,15 +7,35 @@
 //
 
 #import "MatchesViewController.h"
+#import <Parse/Parse.h>
 
-@interface MatchesViewController ()
-
+@interface MatchesViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *companyNameLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation MatchesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
 }
+
+- (void) callArrayWithMatches {
+     NSString *myMatch = self.companyNameLabel.text;
+    
+    //call the array from parse
+    //bring back the name of company
+    PFQuery *query = [PFQuery queryWithClassName:@"GameScore"];
+    [query getObjectInBackgroundWithId:@"xWMyZ4YEGZ" block:^(PFObject *gameScore, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        NSLog(@"%@", gameScore);
+    }];
+    // [myObject fetch];
+}
+
+
 
 @end
